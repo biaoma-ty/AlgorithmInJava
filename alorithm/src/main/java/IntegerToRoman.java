f2bfcdf3646ac84476f006197df899eeb0404bbf
MaBiao <mabiaocas@gmail.com>
2016-05-19 15:26:52 +0800

    14 27.61

---
+++ alorithm/src/main/java/IntegerToRoman.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+/**
+ * Created by F7753 on 5/19/16.
+ * Given an integer, convert it to a roman numeral.
+ * Input is guaranteed to be within the range from 1 to 3999.
+ */
+public class IntegerToRoman {
+    public String intToRoman(int num) {
+
+        return  "I don't know";
+    }
+}
