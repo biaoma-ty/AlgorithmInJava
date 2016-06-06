a4df16318c82f8ef426f77fe14d8beea4c4a30ad
MaBiao <mabiaocas@gmail.com>
2016-06-06 17:17:20 +0800

    326 84.17

--- alorithm/src/main/java/leetcode/PowerofThree.java
+++ alorithm/src/main/java/leetcode/PowerofThree.java
@@ -4,4 +4,20 @@ package leetcode;
  * Created by F7753 on 6/6/16.
  */
 public class PowerofThree {
+    public boolean isPowerOfThree(int n) {
+        while (n > 1) {
+            if (n % 3 != 0)
+                return false;
+            else
+                n = n / 3;
+        }
+        if (n == 1)
+            return true;
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int test = 12;
+        System.out.println(new PowerofThree().isPowerOfThree(test));
+    }
 }
