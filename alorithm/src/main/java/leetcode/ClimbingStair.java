110d80573ce6828fce21bea170421fc34046b93d
MaBiao <mabiaocas@gmail.com>
2016-06-07 09:45:09 +0800

    141 0.34

--- alorithm/src/main/java/leetcode/ClimbingStair.java
+++ alorithm/src/main/java/leetcode/ClimbingStair.java
@@ -8,7 +8,13 @@ public class ClimbingStair {
         int[] dp = {0, 1, 2};
         for(int i = 3; i <= n; i++) {
             dp[i%3] = dp[(i-1)%3] + dp[(i-2)%3];
+            System.out.println(dp[i%3] + " " + dp[(i-1)%3] + " " + dp[(i-2)%3]);
         }
         return dp[n%3];
+    }
+
+    public static void main(String[] args) {
+        ClimbingStair cs = new ClimbingStair();
+        System.out.println(cs.climbStairs(4));
     }
 }
---
+++ alorithm/src/main/java/leetcode/LinkedListCycle.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by F7753 on 6/7/16.
+ */
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> set = new HashSet<ListNode>();
+        int cnt = 0;
+        while (head != null) {
+            set.add(head);
+            System.out.println(head.val);
+            head = head.next;
+            cnt ++;
+            if (cnt > set.size())
+                return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        ListNode node0 = new ListNode(1);
+        ListNode node1 = new ListNode(2);
+        ListNode node2 = new ListNode(3);
+        node0.next = node1;
+        node1.next = node2;
+        node2.next = node0;
+        LinkedListCycle llc = new LinkedListCycle();
+        System.out.println(llc.hasCycle(node0));
+    }
+}
